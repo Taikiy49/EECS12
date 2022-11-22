fa9842b3caa9ffc1c2a0e474243141da8f95e242
Taiki Yamashita <taikiy49@gmail.com>
2022-11-22 13:34:01 -0800

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,36 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: /Users/taikiyamashita/Desktop/EECS 12 HW Assignments/hw2.py =====
+Welcome to the EECS 12 Lucas and Fibonacci convergence experiment!
+Please enter N (>2): 3
+Please enter even K (2<=K<N): 2
+Lucas terms:
+     2
+     1
+     3
+     4
+The Nth term of Lucas is: 4
+
+
+Fibonacci terms:
+     0
+     1
+     1
+     2
+The Nth term of Fibonacci is: 2
+
+
+Golden Ratio: 1.618033988749895
+Fn-k: 1
+Fn+k: 5
+    Left hand side: 6
+Lk: 3
+Fn: 2
+    Right hand side: 6
+F_err: 0.38
+>>> main ()
+Welcome to the EECS 12 Lucas and Fibonacci convergence experiment!
+Please enter N (>2): 2
+Please enter even K (2<=K<N): 1
+Invalid input: K value must be greater than or equal to 2.
